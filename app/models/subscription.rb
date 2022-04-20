8a0f2ae0ac8e74288756d0d4412a6f8eb80b8c38
Parker Lockhart <parkerlockhart42@gmail.com>
2022-04-19 22:52:21 -0500

    Add relationships and relationship testing for tea, subscriptions

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,4 +1,6 @@
 class Subscription < ApplicationRecord
   validates :title, :price, :frequency, presence: true
   enum frequency: { monthly: 0, weekly: 1, seasonal: 2, annual: 3 }
+  has_many :subscription_teas
+  has_many :teas, through: :subscription_teas
 end
