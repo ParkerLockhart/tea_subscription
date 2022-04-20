afdb5595911508058d271c27a991272f77c1dbae
Parker Lockhart <parkerlockhart42@gmail.com>
2022-04-19 23:08:54 -0500

    Create customer table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_20_032900) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_20_040001) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "customers", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_customers_on_email", unique: true
+  end
 
   create_table "subscription_teas", force: :cascade do |t|
     t.integer "status", default: 0
