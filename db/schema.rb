2b92c3c922b5661a53031ccd2f85acd031124248
Parker Lockhart <parkerlockhart42@gmail.com>
2022-04-19 22:16:10 -0500

    Create subscription table, rename tea title column to name

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_19_221054) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_20_025855) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "teas", force: :cascade do |t|
+  create_table "subscriptions", force: :cascade do |t|
     t.string "title"
+    t.integer "price"
+    t.integer "frequency", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "teas", force: :cascade do |t|
+    t.string "name"
     t.string "description"
     t.string "temperature"
     t.string "brew_time"
