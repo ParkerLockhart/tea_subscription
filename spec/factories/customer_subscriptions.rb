3cf7396452e007894767afb29d9affa28ae426d4
Parker Lockhart <parkerlockhart42@gmail.com>
2022-04-20 11:54:39 -0500

    fix typo in customer subscription factory

--- spec/factories/customer_subscriptions.rb
+++ spec/factories/customer_subscriptions.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
-  factory :customer_subscriptions do
+  factory :customer_subscription do
     association :customer, factory: :customer
     association :subscription, factory: :subscription
     status { [0, 1, 2].sample }
   end
-end 
+end
