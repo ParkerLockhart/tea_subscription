7f478023a48d8fb00215eed17ae448535bce2802
Parker Lockhart <parkerlockhart42@gmail.com>
2022-04-19 22:52:21 -0500

    Add relationships and relationship testing for tea, subscriptions

--- app/models/tea.rb
+++ app/models/tea.rb
@@ -1,3 +1,5 @@
 class Tea < ApplicationRecord
   validates :name, :description, :temperature, :brew_time, presence: true
+  has_many :subscription_teas
+  has_many :subscriptions, through: :subscription_teas
 end
