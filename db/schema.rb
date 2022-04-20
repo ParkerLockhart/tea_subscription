5296164356ff5edcf88de6c1a9f590ad48243ff0
Parker Lockhart <parkerlockhart42@gmail.com>
2022-04-19 22:48:24 -0500

    Create subscription_tea joins table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_20_025855) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_20_032900) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "subscription_teas", force: :cascade do |t|
+    t.integer "status", default: 0
+    t.bigint "tea_id", null: false
+    t.bigint "subscription_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["subscription_id"], name: "index_subscription_teas_on_subscription_id"
+    t.index ["tea_id"], name: "index_subscription_teas_on_tea_id"
+  end
 
   create_table "subscriptions", force: :cascade do |t|
     t.string "title"
@@ -31,4 +41,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_20_025855) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "subscription_teas", "subscriptions"
+  add_foreign_key "subscription_teas", "teas"
 end
