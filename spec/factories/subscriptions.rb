c91b769594549d28495e397d36a30c2c2d053a46
Parker Lockhart <parkerlockhart42@gmail.com>
2022-04-19 23:09:47 -0500

    fix typo in subscription factory

--- spec/factories/subscriptions.rb
+++ spec/factories/subscriptions.rb
@@ -1,8 +1,8 @@
 require 'faker'
 FactoryBot.define do
-  factory :Subscription do
+  factory :subscription do
     title { Faker::Space.constellation }
     price {Faker::Number.number(digits: 4) }
     frequency { [0, 1, 2, 3].sample }
   end
-end 
+end
