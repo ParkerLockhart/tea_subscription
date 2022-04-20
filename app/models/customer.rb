cbb132361e0aa04f9e01ecd4cdd342d591724801
Parker Lockhart <parkerlockhart42@gmail.com>
2022-04-19 23:27:14 -0500

    Add relationships to customer, subscription models

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,6 @@
 class Customer < ApplicationRecord
   validates :first_name, :last_name, :email, :address, presence: true
   validates :email, uniqueness: true
+  has_many :customer_subscriptions
+  has_many :subscriptions, through: :customer_subscriptions
 end
