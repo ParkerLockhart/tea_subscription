b58039c5e8d3a6903b07115c814a34fb1aed327b
Parker Lockhart <parkerlockhart42@gmail.com>
2022-04-19 23:27:43 -0500

    Remove status from subscription_tea

--- spec/factories/subscription_teas.rb
+++ spec/factories/subscription_teas.rb
@@ -2,6 +2,5 @@ FactoryBot.define do
   factory :subscription_tea do
     association :subscription, factory: :subscription
     association :tea, factory: :tea
-    status { [0, 1, 2].sample }
   end
 end
