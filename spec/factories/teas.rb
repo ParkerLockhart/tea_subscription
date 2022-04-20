49a3215a9d072be1b9399e54d835ae500a261034
Parker Lockhart <parkerlockhart42@gmail.com>
2022-04-19 22:17:37 -0500

    Update tea tests, model, factory to reflect column rename

--- spec/factories/teas.rb
+++ spec/factories/teas.rb
@@ -1,7 +1,7 @@
 require 'faker'
 FactoryBot.define do
   factory :tea do
-    title { Faker::Tea.variety }
+    name { Faker::Tea.variety }
     description { Faker::Emotion.noun + ' ' + Faker::Dessert.flavor }
     temperature { Faker::Number.between(from: 140, to: 212) + ' ' + "degrees F" }
     brew_time { Faker::Number.between(from: 1, to: 10) + ' ' + "minutes" }
