0477ce8486cac7d5f8643764b4d9a8b5bf373e7c
Parker Lockhart <parkerlockhart42@gmail.com>
2022-04-19 23:27:14 -0500

    Add relationships to customer, subscription models

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -3,4 +3,6 @@ class Subscription < ApplicationRecord
   enum frequency: { monthly: 0, weekly: 1, seasonal: 2, annual: 3 }
   has_many :subscription_teas
   has_many :teas, through: :subscription_teas
+  has_many :customer_subscriptions
+  has_many :customers, through: :customer_subscriptions
 end
