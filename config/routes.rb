5a05bae0a3b3f373be3a6fedf9fef94bf3c3718f
Parker Lockhart <parkerlockhart42@gmail.com>
2022-04-21 23:15:50 -0500

    customer subscription update route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
    namespace :v1 do
      get '/customers/:id/subscriptions', to: 'customer_subscriptions#index'
      post '/customers/:id/subscriptions', to: 'customer_subscriptions#create'
+     patch '/customers/:id/subscriptions/:subscription_id', to: 'customer_subscriptions#update'
    end
  end
 end
