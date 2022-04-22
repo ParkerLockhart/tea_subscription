0df1c16a43eaf24ba8690348a6aa0eb768c635e6
Parker Lockhart <parkerlockhart42@gmail.com>
2022-04-21 21:18:59 -0500

    customer subscription route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
  namespace :api do
    namespace :v1 do
      get '/customers/:id/subscriptions', to: 'customer_subscriptions#index'
+     post '/customers/:id/subscriptions', to: 'customer_subscriptions#create'
    end
  end
 end
