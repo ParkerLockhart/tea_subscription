8c8218d822e9e02772d0e13a33420748b43ba8bf
Parker Lockhart <parkerlockhart42@gmail.com>
2022-04-21 21:20:15 -0500

    customer subscription controller create method

--- app/controllers/api/v1/customer_subscriptions_controller.rb
+++ app/controllers/api/v1/customer_subscriptions_controller.rb
@@ -1,8 +1,20 @@
 class Api::V1::CustomerSubscriptionsController < ApplicationController
-  include ExceptionHandler
-  
+
   def index
     customer = Customer.find(params[:id])
     render json: CustomerSubscriptionSerializer.new(customer.customer_subscriptions)
   end
+
+  def create
+    customer = Customer.find(params[:id])
+    subscription = Subscription.find(params[:subscription_id])
+    subscribe = CustomerSubscription.create(customer: customer, subscription: subscription)
+
+    if subscription.save
+      render json: CustomerSubscriptionSerializer.new(subscribe)
+    else
+      render json: { errors: "Unable to subscribe customer."  }, status: :unprocessable_entity
+    end
+  end
+
 end
