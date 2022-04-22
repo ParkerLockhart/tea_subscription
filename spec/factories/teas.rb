bbe0875656336249814b5b43911fe57b27e75729
Parker Lockhart <parkerlockhart42@gmail.com>
2022-04-21 23:16:23 -0500

    Database seed for postman

--- spec/factories/teas.rb
+++ spec/factories/teas.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
   factory :tea do
     name { Faker::Tea.variety }
     description { Faker::Emotion.noun + ' ' + Faker::Dessert.flavor }
-    temperature { Faker::Number.between(from: 140, to: 212) + ' ' + "degrees F" }
-    brew_time { Faker::Number.between(from: 1, to: 10) + ' ' + "minutes" }
+    temperature { Faker::Number.between(from: 140, to: 212) }
+    brew_time { Faker::Number.between(from: 1, to: 10) }
   end
 end
