2461a4db6c521ac4afe09239f1c70727525a7fab
Parker Lockhart <parkerlockhart42@gmail.com>
2022-04-21 23:15:33 -0500

    customer subscription update testing

--- spec/requests/api/v1/customer_subscription_request_spec.rb
+++ spec/requests/api/v1/customer_subscription_request_spec.rb
@@ -98,4 +98,55 @@ describe 'Customer Subscriptions Endpoint' do
     end
   end
 
+  describe 'PATCH customer subscription' do
+    before(:each) do
+      @customer_subscription = create(:customer_subscription)
+
+      patch "/api/v1/customers/#{@customer_subscription.customer_id}/subscriptions/#{@customer_subscription.subscription_id}", params: { status: "cancelled" }
+    end
+
+    let!(:customer_subscription) { JSON.parse(response.body, symbolize_names: true) }
+
+    it 'returns successful' do
+      expect(response).to be_successful
+    end
+
+    it 'returns correct info' do
+      expect(customer_subscription[:data]).to have_key(:id)
+      expect(customer_subscription[:data][:id]).to be_a(String)
+
+      expect(customer_subscription[:data]).to have_key(:type)
+      expect(customer_subscription[:data][:type]).to eq("customer_subscription")
+
+      expect(customer_subscription[:data]).to have_key(:attributes)
+      expect(customer_subscription[:data][:attributes]).to be_a(Hash)
+
+      attributes = customer_subscription[:data][:attributes]
+
+      expect(attributes).to have_key(:status)
+      expect(attributes[:status]).to be_a(String)
+      expect(attributes[:status]).to eq("cancelled")
+
+      expect(attributes).to have_key(:title)
+      expect(attributes[:title]).to be_a(String)
+
+      expect(attributes).to have_key(:price)
+      expect(attributes[:price]).to be_an(Integer)
+
+      expect(attributes).to have_key(:frequency)
+      expect(attributes[:frequency]).to be_a(String)
+    end
+
+    it 'returns code 404 if customer or subscription not found' do
+      patch '/api/v1/customers/234234/subscriptions/4'
+
+      expect(response.status).to eq(404)
+    end
+
+    it 'returns code 422 if unable to update' do
+      patch "/api/v1/customers/#{@customer_subscription.customer_id}/subscriptions/#{@customer_subscription.subscription_id}", params: { status: "jellied" }
+
+      expect(response.status).to eq(422)
+    end
+  end
 end
