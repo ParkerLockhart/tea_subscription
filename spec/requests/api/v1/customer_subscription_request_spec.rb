74ae3ddceb15dba46c581690274ce08ffa86e8e6
Parker Lockhart <parkerlockhart42@gmail.com>
2022-04-21 21:18:43 -0500

    customer subscription request test

--- spec/requests/api/v1/customer_subscription_request_spec.rb
+++ spec/requests/api/v1/customer_subscription_request_spec.rb
@@ -45,10 +45,57 @@ describe 'Customer Subscriptions Endpoint' do
       end
     end
 
-    it 'returns status 404 if merchant ID invalid' do
+    it 'returns status 404 if customer ID invalid' do
       get "/api/v1/customers/58178116817/subscriptions"
 
       expect(response.status).to eq(404)
-    end 
+    end
   end
+
+  describe 'POST customer subscription' do
+    before(:each) do
+      customer = create(:customer)
+      subscription = create(:subscription)
+
+      post "/api/v1/customers/#{customer.id}/subscriptions", params: { subscription_id: subscription.id }
+    end
+
+    let!(:customer_subscription) { JSON.parse(response.body, symbolize_names: true) }
+
+    it 'returns successful' do
+      expect(response).to be_successful
+    end
+
+    it 'returns correct info' do
+      expect(customer_subscription[:data]).to have_key(:id)
+      expect(customer_subscription[:data][:id]).to be_a(String)
+
+      expect(customer_subscription[:data]).to have_key(:type)
+      expect(customer_subscription[:data][:type]).to eq("customer_subscription")
+
+      expect(customer_subscription[:data]).to have_key(:attributes)
+      expect(customer_subscription[:data][:attributes]).to be_a(Hash)
+
+      attributes = customer_subscription[:data][:attributes]
+
+      expect(attributes).to have_key(:status)
+      expect(attributes[:status]).to be_a(String)
+
+      expect(attributes).to have_key(:title)
+      expect(attributes[:title]).to be_a(String)
+
+      expect(attributes).to have_key(:price)
+      expect(attributes[:price]).to be_an(Integer)
+
+      expect(attributes).to have_key(:frequency)
+      expect(attributes[:frequency]).to be_a(String)
+    end
+
+    it 'returns code 404 if customer or subscription not found' do
+      post "/api/v1/customers/234234/subscriptions"
+
+      expect(response.status).to eq(404)
+    end
+  end
+
 end
